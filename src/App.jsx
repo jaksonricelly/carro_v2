24aa5c768691c4541660b311b42f38e017801f3f
jaksonricelly <jaksonricelly@gmail.com>
2026-04-03 13:59:50 -0300

    segundo commit da responsividade ajustes no main

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import iconSuvs from "./assets/icon-suvs.svg"
 import iconLuxuosos from "./assets/icon-luxury.svg"
 export default function App() {
   return (
-    <main className="min-h-svh bg-off-white-bg py-20 px-6 antialiased">
-      <section className="lg:flex justify-center items-center">
-        <div className="bg-orange p-12 rounded-t-lg">
+    <main className="min-h-svh bg-off-white-bg py-20 px-6 antialiased flex justify-center items-center">
+      <section className="lg:flex">
+        <div className="bg-orange p-12 max-lg:rounded-t-lg lg:rounded-l-lg">
           <img src={iconSedans} alt="Icone dos carros" className="mb-7"/>
           <h2 className="font-big-shoulders mb-7 uppercase text-4xl text-off-white-bg font-bold">Sedans</h2>
           <p className="mb-7 font-lexend text-offwhite">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
@@ -17,7 +17,7 @@ export default function App() {
           <p className="mb-7 font-lexend text-offwhite">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
           <a href="https://www.webmotors.com.br/suvs" target="_blank" className="block w-fit px-10 py-3 rounded-3xl bg-off-white-bg text-green font-bold">Ver mais</a>
         </div>
-        <div className="bg-dark-green p-12 rounded-b-lg">
+        <div className="bg-dark-green p-12 max-lg:rounded-b-lg lg:rounded-r-lg">
           <img src={iconLuxuosos} alt="Icone dos carros luxuosos" className="mb-7"/>
           <h2 className="font-big-shoulders mb-7 uppercase text-4xl text-off-white-bg font-bold">Luxuosos</h2>
           <p className="mb-7 font-lexend text-offwhite">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
