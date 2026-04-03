a4f054ee286ea2c0c69a8cbdd44c897a8b62207c
jaksonricelly <jaksonricelly@gmail.com>
2026-04-03 11:58:59 -0300

    terceiro commit do app css fontes importadas

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,16 @@ export default function App() {
         <div className="bg-orange">
           <img src={iconSedans} alt="Icone dos carros"/>
           <h2>Sedans</h2>
-          <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <p className="font-big-shoulders">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
           <a href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
         </div>
-        <div>
+        <div className="bg-green">
           <img src={iconSuvs} alt="Icone dos carros Suvs" />
           <h2>SUVs</h2>
           <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
           <a href="https://www.webmotors.com.br/suvs" target="_blank">Ver mais</a>
         </div>
-        <div>
+        <div className="bg-dark-green">
           <img src={iconLuxuosos} alt="Icone dos carros luxuosos" />
           <h2>Luxuosos</h2>
           <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
